e7c4cbf388d7ec6be0a56a4c683a6e4c18738042
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-14 17:07:02 +0800

    实现了表达式的转换

---
+++ small_step/main.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+class Number < Struct.new(:value)
+  def to_s
+    value.to_s
+  end
+
+  def inspect
+    "«#{self}»"
+  end
+
+  def reducible?
+    false
+  end
+end
+
+class Add < Struct.new(:left, :right)
+  def to_s
+    "#{left} + #{right}"
+  end
+
+  def inspect
+    "«#{self}»"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce
+    if left.reducible?
+      Add.new(left.reduce, right)
+    elsif right.reducible?
+      Add.new(left, right.reduce)
+    else
+      Number.new(left.value + right.value)
+    end
+  end
+
+end
+
+class Multiply < Struct.new(:left, :right)
+  def to_s
+    "#{left} * #{right}"
+  end
+
+  def inspect
+    "«#{self}»"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce
+    if left.reducible?
+      Multiply.new(left.reduce, right)
+    elsif right.reducible?
+      Multiply.new(left, right.reduce)
+    else
+      Number.new(left.value * right.value)
+    end
+  end
+
+end
+
+expression = Add.new(
+Multiply.new(Number.new(1), Number.new(2)),
+Multiply.new(Number.new(3), Number.new(4))
+)
+puts expression
+
+while expression.reducible?
+    expression = expression.reduce()
+    puts expression 
+end
