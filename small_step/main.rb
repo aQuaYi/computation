a08337ab41d90637b0c31b4a02c5edf9d54e7335
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-15 10:08:39 +0800

    small step: 添加了环境变量

--- small_step/main.rb
+++ small_step/main.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Number < Struct.new(:value)
   def to_s
     value.to_s
@@ -27,15 +25,16 @@ class Add < Struct.new(:left, :right)
     true
   end
 
-  def reduce
+  def reduce(environment)
     if left.reducible?
-      Add.new(left.reduce, right)
+      Add.new(left.reduce(environment), right)
     elsif right.reducible?
-      Add.new(left, right.reduce)
+      Add.new(left, right.reduce(environment))
     else
       Number.new(left.value + right.value)
     end
   end
+
 end
 
 class Multiply < Struct.new(:left, :right)
@@ -51,15 +50,16 @@ class Multiply < Struct.new(:left, :right)
     true
   end
 
-  def reduce
+  def reduce(environment)
     if left.reducible?
-      Multiply.new(left.reduce, right)
+      Multiply.new(left.reduce(environment), right)
     elsif right.reducible?
-      Multiply.new(left, right.reduce)
+      Multiply.new(left, right.reduce(environment))
     else
       Number.new(left.value * right.value)
     end
   end
+
 end
 
 class Boolean < Struct.new(:value)
@@ -69,7 +69,7 @@ class Boolean < Struct.new(:value)
 
   def inspect
     "«#{self}»"
- end
+  end
 
   def reducible?
     false
@@ -89,21 +89,41 @@ class LessThan < Struct.new(:left, :right)
     true
   end
 
-  def reduce
+  def reduce(environment)
     if left.reducible?
-      LessThan.new(left.reduce, right)
+      LessThan.new(left.reduce(environment), right)
     elsif right.reducible?
-      LessThan.new(left, right.reduce)
+      LessThan.new(left, right.reduce(environment))
     else
       Boolean.new(left.value < right.value)
     end
   end
+
+end
+
+class Variable < Struct.new(:name)
+  def to_s
+    name.to_s
+  end
+
+  def inspect
+    "«#{self}»"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    environment[name]
+  end
 end
 
 #------------------------------------------------------
-class Machine < Struct.new(:expression)
+
+class Machine < Struct.new(:expression, :environment)
   def step
-    self.expression = expression.reduce
+    self.expression = expression.reduce(environment)
   end
 
   def run
@@ -116,12 +136,17 @@ class Machine < Struct.new(:expression)
 end
 
 Machine.new(
-  Add.new(
-    Multiply.new(Number.new(1), Number.new(2)),
-    Multiply.new(Number.new(3), Number.new(4))
-  )
+    Add.new(
+        Multiply.new(Number.new(1), Number.new(2)),
+        Multiply.new(Number.new(3), Number.new(4))
+    )
 ).run
 
 Machine.new(
-  LessThan.new(Number.new(5), Add.new(Number.new(2), Number.new(2)))
+    LessThan.new(Number.new(5), Add.new(Number.new(2), Number.new(2)))
+).run
+
+Machine.new(
+    Add.new(Variable.new(:x), Variable.new(:y)),
+    {x: Number.new(3), y: Number.new(4)}
 ).run
